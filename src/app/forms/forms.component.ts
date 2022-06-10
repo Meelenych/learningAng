0cead78d334a30c8157a8b66cdb366d4a4a3d301
Taras Demchenko <taras.demchenko@advantiss.com>
2022-06-10 11:55:18 +0300

    forms validation

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./forms.component.css']
 })
 export class FormsComponent {
+
+  // ViewChild
+  @ViewChild('myForm') myForm: NgForm;
 
   answers = [
     {
@@ -21,12 +24,21 @@ export class FormsComponent {
 
   registerData = [];
 
-  submitForm(myForm: NgForm) {
-    console.log(
-      'submitted', myForm.value
-    )
-    this.registerData.push(myForm.value)
+
+  // ViewChild
+  submitForm() {
+    console.log('submitted', this.myForm)
+    this.registerData.push(this.myForm.value)
     console.log(this.registerData)
-  };
+  }
+
+
+  // submitForm(myForm: NgForm) {
+  //   console.log(
+  //     'submitted', myForm.value
+  //   )
+  //   this.registerData.push(myForm.value)
+  //   console.log(this.registerData)
+  // };
 
 }
